9b1f0a876dc31e25c7b531f026fb10c561cf5cff
xavpul <xavier@xavierp.dev>
2025-08-10 23:49:43 +0800

    Implemented hook that loads spatialite for every new db connection

--- core/db.py
+++ core/db.py
@@ -1,3 +1,4 @@
+from sqlalchemy import event
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
@@ -10,9 +11,24 @@ from core.config import settings
 engine: AsyncEngine = create_async_engine(
     str(settings.DB_URL),
     echo=True,
+    connect_args={
+        "check_same_thread": False,
+        "detect_types": 0,
+        "timeout": 30,
+        "uri": True,
+    },
+    future=True,
     pool_size=10,
     max_overflow=20,
 )
+
+
+def _enable_spatialite(dbapi_conn, _):
+    dbapi_conn.enable_load_extension(True)
+    dbapi_conn.load_extension("/usr/lib/sqlite3/pmod_spatialite.so")
+
+
+event.listen(engine.sync_engine, "connect", _enable_spatialite)
 
 SessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(
     bind=engine,
